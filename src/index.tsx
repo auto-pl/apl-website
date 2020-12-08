35bde192dd047e7c64d90753bbeaede8d07dc3b3
Leonhard S <leonhard-sei@outlook.com>
2020-12-08 20:36:50 +0100

    Fix capitalisation

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App/App";
-import reportWebVitals from "./App/reportWebVitals";
+import App from "./app/App";
+import reportWebVitals from "./app/reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
