5293e28e6dda80f6f628f8b7d85169419d0e9306
SMC242 <smc242@users.noreply.github.com>
2020-12-08 12:04:58 +0000

    Moved the dummy value out of the function

--- src/Utils/api_interface.ts
+++ src/Utils/api_interface.ts
@@ -4,71 +4,73 @@ interface apiT {
   get_all_continents: ContinentGetter;
 }
 
+const dummy_continents = [
+  {
+    name: "Indar",
+    population: 410,
+    locked: false,
+    base_states: [
+      {
+        name: "Hvar",
+        faction: "TR",
+        time_held: 1000,
+        captured_by: null,
+        continent_id: CI.Indar,
+        leaderboard: null,
+      },
+    ],
+  },
+  {
+    name: "Amerish",
+    population: 90,
+    locked: false,
+    base_states: [
+      {
+        name: "Heyoka Chemical Lab",
+        faction: "VS",
+        time_held: 1000,
+        captured_by: null,
+        continent_id: CI.Amerish,
+        leaderboard: null,
+      },
+    ],
+  },
+  {
+    name: "Esamir",
+    population: 780,
+    locked: false,
+    base_states: [
+      {
+        name: "Pale Canyon Chemical",
+        faction: "NC",
+        time_held: 1000,
+        captured_by: null,
+        continent_id: CI.Esamir,
+        leaderboard: null,
+      },
+    ],
+  },
+  {
+    name: "Hossin",
+    population: 0,
+    locked: true,
+    base_states: [
+      {
+        name: "Chac Fusion",
+        faction: "NS",
+        time_held: 1000,
+        captured_by: null,
+        continent_id: CI.Hossin,
+        leaderboard: null,
+      },
+    ],
+  },
+];
+
 export const api: apiT = {
   get_all_continents: (key) => {
     // make api call for all continents
-    const result = [
-      {
-        name: "Indar",
-        population: 410,
-        locked: false,
-        base_states: [
-          {
-            name: "Hvar",
-            faction: "TR",
-            time_held: 1000,
-            captured_by: null,
-            continent_id: CI.Indar,
-            leaderboard: null,
-          },
-        ],
-      },
-      {
-        name: "Amerish",
-        population: 90,
-        locked: false,
-        base_states: [
-          {
-            name: "Heyoka Chemical Lab",
-            faction: "VS",
-            time_held: 1000,
-            captured_by: null,
-            continent_id: CI.Amerish,
-            leaderboard: null,
-          },
-        ],
-      },
-      {
-        name: "Esamir",
-        population: 780,
-        locked: false,
-        base_states: [
-          {
-            name: "Pale Canyon Chemical",
-            faction: "NC",
-            time_held: 1000,
-            captured_by: null,
-            continent_id: CI.Esamir,
-            leaderboard: null,
-          },
-        ],
-      },
-      {
-        name: "Hossin",
-        population: 0,
-        locked: true,
-        base_states: [
-          {
-            name: "Chac Fusion",
-            faction: "NS",
-            time_held: 1000,
-            captured_by: null,
-            continent_id: CI.Hossin,
-            leaderboard: null,
-          },
-        ],
-      },
-    ];
+    const result = dummy_continents;
     return result.filter(key);
   },
 };
