51f9feffcb011302e1b13b036586af666e1173e9
SMC242 <smc242@users.noreply.github.com>
2020-12-09 12:45:11 +0000

    Driver code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { MapSwitcher } from "../Components/navigation/map_switcher";
+import { api } from "../Utils/api_interface";
+const all_conts = api.get_all_continents();
+const conts = all_conts.map((cont) => ({ details: cont, view_url: "ayaya" }));
 
 function App() {
   return (
@@ -19,6 +24,11 @@ function App() {
           Learn React
         </a>
       </header>
+
+      {/* put your tests here */}
+      <div id="testing-code-goes-here">
+        <MapSwitcher continents={conts} />
+      </div>
     </div>
   );
 }
