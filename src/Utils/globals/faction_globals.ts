6043db4dcb4b3804ba9d24a98dad7b72db9107f8
SMC242 <smc242@users.noreply.github.com>
2020-12-09 09:23:36 +0000

    Added faction stuff

---
+++ src/Utils/globals/faction_globals.ts
@@ -0,0 +1,10 @@
+export const vs_colour = "rgba(92, 77, 105, 0.5)";
+export const tr_colour = "rgba(135, 81, 67, 0.5)";
+export const nc_colour = "rgba(87, 129, 143, 0.5)";
+export const ns_colour = "rgba(10, 16, 3, 0.5)";
+
+export const faction_colours = new Map([
+  ["TR", tr_colour],
+  ["NC", nc_colour],
+  ["VS", vs_colour],
+]);
