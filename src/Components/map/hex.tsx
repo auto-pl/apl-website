5ed6613eca11921bf9f73f636cf7b7b0dadc6fc0
SMC242 <mcalonansam2@gmail.com>
2020-12-14 11:48:55 +0000

    Starting work on Hex

--- src/Components/map/hex.tsx
+++ src/Components/map/hex.tsx
@@ -1,1 +1,16 @@
-export const plscompile = {};
+import React, { FC, useState, memo, useReducer } from "react";
+import { BaseState } from "../../interfaces/continent";
+
+interface HexProps {
+  base_state: BaseState;
+}
+
+const check_for_hex_update = (base_state: BaseState) => true;
+
+const Hex: FC<HexProps> = memo((props) => {
+  return (
+    <div>
+      <span></span>
+    </div>
+  );
+});
---
+++ src/Components/map/hover_menu.tsx
@@ -0,0 +1,2 @@
+export const plscompile = {};
+// this file will have the hover menu for hexes
