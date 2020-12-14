42d2330b0d0e028ccadd3b0461c327152bebdbc7
SMC242 <mcalonansam2@gmail.com>
2020-12-14 11:33:11 +0000

    Increased opacity

--- src/Utils/globals/faction_globals.ts
+++ src/Utils/globals/faction_globals.ts
@@ -2,10 +2,10 @@ import TR_logo from "../../app/Resources/Images/Logos/TR.png";
 import VS_logo from "../../app/Resources/Images/Logos/VS.png";
 import NC_logo from "../../app/Resources/Images/Logos/NC.png";
 
-export const vs_colour = "rgba(92, 77, 105, 0.5)";
-export const tr_colour = "rgba(135, 81, 67, 0.5)";
-export const nc_colour = "rgba(87, 129, 143, 0.5)";
-export const ns_colour = "rgba(10, 16, 3, 0.5)";
+export const vs_colour = "rgba(92, 77, 105, 0.75)";
+export const tr_colour = "rgba(135, 81, 67, 0.75)";
+export const nc_colour = "rgba(87, 129, 143, 0.75)";
+export const ns_colour = "rgba(10, 16, 3, 0.75)";
 
 export const faction_colours = new Map([
   ["TR", tr_colour],
