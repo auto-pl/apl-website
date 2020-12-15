3900e1d52719f838ab57d4921f5b54811f3a3579
SMC242 <mcalonansam2@gmail.com>
2020-12-15 10:32:45 +0000

    HoverMenu interfaces

--- src/Components/map/hover_menu.tsx
+++ src/Components/map/hover_menu.tsx
@@ -1,2 +1,29 @@
-export const plscompile = {};
 // this file will have the hover menu for hexes
+import React, { FC } from "react";
+
+interface HoverMenuProps {
+  /**
+   * The name of the menu to be placed at the top
+   */
+  title: string;
+  /**
+   * What should be displayed in the body.
+   */
+  body_items: Array<FC>;
+
+  options?: {
+    /**
+     * Whether the menu should appear in a set spot.
+     * Should be one of [bottom right, bottom left, top right, top left]
+     */
+    fixed_position?: string;
+  };
+}
+
+export const HoverMenu: FC<HoverMenuProps> = (props: HoverMenuProps) => {
+  return (
+    <div>
+      <span></span>
+    </div>
+  );
+};
