b6b16a3c2a6decef46e9761f2f31f2832e77ce55
SMC242 <mcalonansam2@gmail.com>
2020-12-16 13:21:45 +0000

    Did the positioning for HoverMenu

--- src/Components/map/hover_menu.tsx
+++ src/Components/map/hover_menu.tsx
@@ -1,5 +1,5 @@
 // this file will have the hover menu for hexes
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, CSSProperties } from "react";
 
 type position = "bottom right" | "bottom left" | "top right" | "top left";
 
@@ -22,9 +22,57 @@ interface HoverMenuProps {
   };
 }
 
+const get_position_style = (
+  options: HoverMenuProps["options"]
+): CSSProperties => {
+  const default_style: CSSProperties = {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    top: 0,
+  };
+  if (!options) return default_style;
+
+  const dimensions = { height: window.outerHeight, width: window.outerWidth };
+  // get 90% of the dimension
+  const reduce_dimension = (n: number): number => n - 0.1 * n;
+  switch (options.fixed_position) {
+    case undefined:
+      return default_style;
+
+    case "bottom right":
+      return {
+        top: reduce_dimension(dimensions.height),
+        left: reduce_dimension(dimensions.width),
+      };
+
+    case "top right":
+      return {
+        bottom: reduce_dimension(dimensions.height),
+        left: reduce_dimension(dimensions.width),
+      };
+
+    case "bottom left":
+      return {
+        top: reduce_dimension(dimensions.height),
+        right: reduce_dimension(dimensions.width),
+      };
+
+    case "top left":
+      return {
+        bottom: reduce_dimension(dimensions.height),
+        right: reduce_dimension(dimensions.width),
+      };
+
+    default:
+      return default_style;
+  }
+};
+
 export const HoverMenu: FC<HoverMenuProps> = (props: HoverMenuProps) => {
   return (
-    <div>
+    <div style={get_position_style(props.options)}>
       <span>{props.title}</span>
       <div>{props.body_items}</div>
     </div>
