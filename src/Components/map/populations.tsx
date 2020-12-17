f629d4138dfde7032d5bdcc181f4e03bd5892c46
SMC242 <mcalonansam2@gmail.com>
2020-12-17 10:12:57 +0000

    Much of the code wasn't necessary

--- src/Components/map/populations.tsx
+++ src/Components/map/populations.tsx
@@ -6,35 +6,36 @@ interface PopulationPieChartProps {
    * The number of players per faction
    */
   populations: { nc: number; vs: number; tr: number };
+  /**
+   * Defaults to 150px
+   */
+  height?: number;
+  /**
+   * Defaults to 200px
+   */
+  width?: number;
 }
 
 const PopulationPieChart: FC<PopulationPieChartProps> = (props) => {
   const total_pop = Object.values(props.populations).reduce(
     (acc, item) => acc + item
   );
-
-  const array_to_obj = (
-    obj: { [faction_name: string]: number },
-    [key, value]: [string, number]
-  ) => {
-    obj[key] = value;
-    return obj;
-  };
   const percentages = Object.entries(props.populations).map(([name, pop]) => [
     name,
-    (pop / total_pop) * 100,
+    ((pop / total_pop) * 100).toString().concat("%"),
   ]);
 
-  // !FIX: this type cast is a hack
-  // The compiler was saying `percentages` was `ReactText`
-  const faction_percentages = (percentages as Array<[string, number]>).reduce(
-    array_to_obj,
-    {}
-  );
-
+  const options = {
+    title: "Faction populations",
+  };
   return (
-    <div>
-      <span></span>
-    </div>
+    <Chart
+      chartType="PieChart"
+      height={props.height || "150px"}
+      width={props.width || "200px"}
+      loader={<div>ben was here</div>}
+      data={percentages}
+      options={options}
+    />
   );
 };
