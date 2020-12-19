8511c59b6d8432383a88e57707316277e42a8d2f
SMC242 <smc242@users.noreply.github.com>
2020-12-19 23:00:25 +0000

    The script didn't work properly but I fixed it

--- src/Components/svg_wrappers/error.tsx
+++ src/Components/svg_wrappers/error.tsx
@@ -9,9 +9,12 @@ export const Error = (props: React.SVGProps<SVGSVGElement>) => (
     viewBox="0 0 32 32"
     {...props}
   >
-    <g fill="currentColor">
+    <g fill="#D72828">
       <circle cx={16} cy={16} r={16} />
-      <path d="M14.5 25h3v-3h-3v3zm0-19v13h3V6h-3z" />
+      <path
+        d="M14.5 25h3v-3h-3v3zm0-19v13h3V6h-3z"
+        style={{ fill: "#E6E6E6" }}
+      />
     </g>
   </svg>
 );
