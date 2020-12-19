dcad6c5dea8b11ed321707a67b89ceee2229ac61
SMC242 <mcalonansam2@gmail.com>
2020-12-19 20:19:41 +0000

    Removed the default stuff

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,21 +12,6 @@ function App() {
   const [hovered, set_hover] = React.useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
       {/* put your tests here */}
       <div id="testing-code-goes-here">
         <Hex
