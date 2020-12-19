00b4c2f424fc2b5de05966eb2b9d687e3a89832c
SMC242 <smc242@users.noreply.github.com>
2020-12-19 23:29:59 +0000

    Blasted some unused imports

--- src/Components/map/hex_img.tsx
+++ src/Components/map/hex_img.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC } from "react";
 import { Error } from "../svg_wrappers/error";
 
 // TODO: (blocked) get the hexes from the database
