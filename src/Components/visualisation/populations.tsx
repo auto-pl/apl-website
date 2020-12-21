90e47732dd6d5950145068dea503918c69dc8d29
SMC242 <smc242@users.noreply.github.com>
2020-12-21 21:50:42 +0000

    The chart works now

--- src/Components/visualisation/populations.tsx
+++ src/Components/visualisation/populations.tsx
@@ -1,6 +1,6 @@
 import React, { FC, CSSProperties } from "react";
 import { Chart } from "react-google-charts";
-import { faction_colours } from "../../Utils/globals/faction_globals";
+import { get_faction_colour } from "../../Utils/globals/faction_globals";
 
 interface PopulationProps {
   /**
@@ -21,25 +21,24 @@ interface PopulationPieChartProps extends PopulationProps {
 }
 
 export const PopulationPieChart: FC<PopulationPieChartProps> = (props) => {
-  const total_pop = Object.values(props.populations).reduce(
-    (acc, item) => acc + item
-  );
-  const percentages = Object.entries(props.populations).map(([name, pop]) => [
-    name,
-    ((pop / total_pop) * 100).toString().concat("%"),
-  ]);
-
+  const rows: Array<[string, any]> = [
+    ["Faction", "Number of players"] as [string, any],
+  ].concat(Object.entries(props.populations));
   const options = {
-    title: "Faction populations",
+    title: "Faction population",
+    slices: {
+      0: { color: get_faction_colour(rows[1][0], true) },
+      1: { color: get_faction_colour(rows[2][0], true) },
+      2: { color: get_faction_colour(rows[3][0], true) },
+    },
   };
-  //!FIX: chart doesn't work
   return (
     <Chart
       chartType="PieChart"
       height={props.height || "200px"}
       width={props.width || "250px"}
       loader={<div>ben was here</div>}
-      data={percentages}
+      data={rows}
       options={options}
     />
   );
@@ -51,7 +50,10 @@ export const EnemiesDetected: FC<PopulationProps> = (props) => {
   return (
     <div>
       {Object.entries(props.populations).map(([name, pop], i) => (
-        <p style={{ ...p_style, backgroundColor: faction_colours.get(name) }}>
+        <p
+          key={i}
+          style={{ ...p_style, backgroundColor: get_faction_colour(name) }}
+        >
           {name} detected: {pop}
         </p>
       ))}
