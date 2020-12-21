f3a8028dbb5559fa71855a4baf13c3317a808842
SMC242 <smc242@users.noreply.github.com>
2020-12-21 21:55:07 +0000

    Starting work on Continent tomorrow

--- src/Components/map/continent.tsx
+++ src/Components/map/continent.tsx
@@ -1,1 +1,5 @@
+import { Hex } from "./hex/hex";
 export const plscompile = {};
+
+// TODO: map over the continent's bases and send the states to `Hex`
+// TODO: add population ratio at the top right
