de9f52b684c9f65432e2a47cc9b069f3a68c92e8
SMC242 <smc242@users.noreply.github.com>
2020-12-27 03:30:20 +0000

    Moving to leaflet

--- src/Components/map/map.tsx
+++ src/Components/map/map.tsx
@@ -1,1 +1,20 @@
-export const plscompile = {};
+import React, { FC, useReducer } from "react";
+import { ContinentDetails } from "../../interfaces/continent";
+import {
+  MapContainer,
+  MapConsumer,
+  Polyline,
+  useMap,
+  useMapEvent,
+} from "react-leaflet";
+
+interface Continent extends ContinentDetails {
+  /**
+   * The URL to the view for the continent
+   */
+  view_url: string;
+}
+
+interface MapProps {
+  continents: Array<Continent>;
+}
