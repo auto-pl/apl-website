ccd928dbb27e913bbb39c0c675008d936967bb0b
SMC242 <smc242@users.noreply.github.com>
2020-12-30 07:55:46 +0000

    Testing driver code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,15 @@
 import React from "react";
-import {
-  MapSwitcher,
-  MSContinents,
-} from "../Components/navigation/map_switcher";
+import { Map } from "../Components/map/map";
 
 import { api } from "../Utils/api_interface";
 const conts = api.get_all_continents();
-const continents = conts.reduce(
-  (acc: MSContinents, cont) => [...acc, { details: cont, view_url: "foo" }],
-  []
-);
 
 function App() {
   return (
     <div>
       {/* put your tests here */}
       <div id="testing-code-goes-here">
-        <MapSwitcher continents={continents} />
+        <Map continents={conts} view_urls={["", "", "", ""]} />
       </div>
     </div>
   );
