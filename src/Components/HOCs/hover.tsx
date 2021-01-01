f8482048e9645e2d429791ab42c44a16eb4d92e8
SMC242 <smc242@users.noreply.github.com>
2021-01-01 02:26:17 +0000

    Gave up on an abstract HOC. Made a specific one as practice

--- src/Components/HOCs/hover.tsx
+++ src/Components/HOCs/hover.tsx
@@ -1,57 +1,39 @@
 import React, { ComponentType, useState, FC } from "react";
 import { HoverMenu, HoverMenuProps } from "../menus/hover_menu";
+import { BaseState } from "../../interfaces/continent";
 import { PopulationReport } from "../visualisation/populations";
 
-type WithHoverMenuType = <TargetProps extends object>(
-  TargetComponent: ComponentType<TargetProps>,
-  hover_menu_props: HoverMenuProps
-) => FC<HoverMenuProps & TargetProps>;
+export interface WithBaseHoverMenuProps {
+  base_state: BaseState;
+}
 
-export const WithHoverMenu: WithHoverMenuType = (
-  TargetComponent,
-  hover_menu_props: HoverMenuProps
-) => {
-  return (props) => {
+/**
+ * Add a `HoverMenu` to the component. The menu will contain information about a base
+ * @param TargetComponent The component to add the `HoverMenu` to
+ */
+export function WithBaseHoverMenu<TargetProps extends WithBaseHoverMenuProps>(
+  TargetComponent: ComponentType<TargetProps>
+): FC<TargetProps & WithBaseHoverMenuProps> {
+  const ComponentWithBaseHoverMenu = (props: TargetProps) => {
     const [hovered, set_hovered] = useState(false);
     return (
-      <div>
+      <div
+        onMouseEnter={() => set_hovered(true)}
+        onMouseLeave={() => set_hovered(false)}
+      >
         <TargetComponent {...props} />
-        {props.hovered ? <HoverMenu /> : null}
+        {hovered ? (
+          <HoverMenu
+            title={props.base_state.name}
+            body_items={[
+              <PopulationReport populations={props.base_state.population} />,
+            ]}
+            options={{ fixed_position: "bottom right" }}
+          />
+        ) : null}
       </div>
     );
   };
-};
 
-/*
-export const Hex = memo<HexProps>((props) => {
-  const [state, dispatch] = useReducer(reducer, {
-    base_state: props.base_state,
-    hovered: false,
-  });
-  // !Saved for V2
-  // const priority_style = get_priority_style(props.base_state.priority_level);
-  const priority_style = "";
-
-  return (
-    <div>
-      <div
-        id={`Hex_continent=${props.base_state.continent_id}_id=${props.base_state.id}`}
-      ></div>
-      <HexImg
-        id={props.base_state.id}
-        priority_class_name={priority_style}
-        hover={() => dispatch(ACTION_TYPES.hovered)}
-      />
-      {state.hovered ? (
-        <HoverMenu
-          title={props.base_state.name}
-          body_items={[
-            <PopulationReport populations={props.base_state.population} />,
-          ]}
-          options={{ fixed_position: "bottom right" }}
-        />
-      ) : null}
-    </div>
-  );
-}, check_for_hex_update);
-*/
+  return ComponentWithBaseHoverMenu;
+}
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { Map } from "../Components/map/map";
+import { WithBaseHoverMenu } from "../Components/HOCs/hover";
 
 import { api } from "../Utils/api_interface";
+import { BaseState } from "../interfaces/continent";
 const conts = api.get_all_continents();
+
+const TestComponent = WithBaseHoverMenu((props: { base_state: BaseState }) => {
+  return (
+    <div>
+      <span>Test</span>
+    </div>
+  );
+});
 
 function App() {
   return (
     <div>
       {/* put your tests here */}
       <div id="testing-code-goes-here">
-        <Map continents={conts} view_urls={["", "", "", ""]} />
+        <TestComponent base_state={conts[0].base_states[0]} />
+        {/*
+          <Map continents={conts} view_urls={["", "", "", ""]} />
+        */}
       </div>
     </div>
   );
