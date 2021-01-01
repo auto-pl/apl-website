f57dd1a001fb13372df7d9c7a022e401ca568b6e
SMC242 <smc242@users.noreply.github.com>
2021-01-01 19:32:06 +0000

    Testing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,27 @@
 import React from "react";
-import { Map } from "../Components/map/map";
-import { WithBaseHoverMenu } from "../Components/HOCs/hover";
+import { Button, ToggleButton } from "../Components/inputs/buttons/ps2button";
 
 import { api } from "../Utils/api_interface";
-import { BaseState } from "../interfaces/continent";
 const conts = api.get_all_continents();
-
-const TestComponent = WithBaseHoverMenu((props: { base_state: BaseState }) => {
-  return (
-    <div>
-      <span>Test</span>
-    </div>
-  );
-});
 
 function App() {
   return (
     <div>
       {/* put your tests here */}
       <div id="testing-code-goes-here">
-        <TestComponent base_state={conts[0].base_states[0]} />
         {/*
           <Map continents={conts} view_urls={["", "", "", ""]} />
         */}
+        <Button
+          text="test"
+          on_click={() => console.log("Clicked")}
+          tooltip_text="Hovered"
+        />
+        <Button />
+        <ToggleButton
+          text="toggle test"
+          on_click={() => console.log("toggle clicked")}
+        />
       </div>
     </div>
   );
