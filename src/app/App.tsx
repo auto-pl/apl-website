b4f4d699bdaddc7c6dafd953b8d9f9acf5da1d1b
SMC242 <smc242@users.noreply.github.com>
2021-01-02 19:28:16 +0000

    Testing driver code
    The width doesn't work

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { DropdownMenu } from "../Components/inputs/ps2dropdown";
 import "../styles/global/ps2_styles/text.css";
 import "../styles/global/ps2_styles/background.css";
 import "../styles/global/ps2_styles/containers.css";
-import Amerish from "./Resources/Images/Maps/Amerish/LOD0.png";
+
+import { TextContainer } from "../Components/containers/text_container";
+import "../styles/global/ps2_styles/positioning.css";
 
 import { api } from "../Utils/api_interface";
 const conts = api.get_all_continents();
@@ -12,15 +13,14 @@ function App() {
   return (
     <div className="background wrapper">
       {/* put your tests here */}
-      <div className="container" id="testing-code-goes-here">
-        <DropdownMenu
-          label="Testing"
-          items={[
-            { label: "hii", value: "I'm bob" },
-            { label: "hello", value: "bob" },
-          ]}
-          on_change={(v) => console.log(v)}
-        />
+      <div className="left" id="testing-code-goes-here">
+        <TextContainer
+          width={20}
+          header_settings={{ text: "The best outfit ever" }}
+        >
+          Join MUMS on Cobalt VS. It is the greatest outfit to have ever graced
+          Cobaltium.
+        </TextContainer>
       </div>
     </div>
   );
