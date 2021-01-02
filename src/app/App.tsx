6ac45b6f364cd3b6c0065d2d64605f669812b009
SMC242 <smc242@users.noreply.github.com>
2021-01-02 06:31:25 +0000

    OOOOOOOOOOOOOOOOH fancy styles whoooooa

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,18 @@
 import React from "react";
-import { Button, ToggleButton } from "../Components/inputs/buttons/ps2button";
+import { DropdownMenu } from "../Components/inputs/ps2dropdown";
+import "../styles/global/ps2_styles/text.css";
+import "../styles/global/ps2_styles/background.css";
+import "../styles/global/ps2_styles/containers.css";
+import Amerish from "./Resources/Images/Maps/Amerish/LOD0.png";
 
 import { api } from "../Utils/api_interface";
 const conts = api.get_all_continents();
 
 function App() {
   return (
-    <div>
+    <div className="background wrapper">
       {/* put your tests here */}
-      <div id="testing-code-goes-here">
-        {/*
-          <Map continents={conts} view_urls={["", "", "", ""]} />
-        */}
-        <Button
-          text="test"
-          on_click={() => console.log("Clicked")}
-          tooltip_text="Hovered"
-          value="yes"
-        />
-        <Button />
-        <ToggleButton
-          text="toggle test"
-          on_click={() => console.log("toggle clicked")}
-          on_value="v"
-          off_value="5"
-        />
-      </div>
+      <div className="container" id="testing-code-goes-here"></div>
     </div>
   );
 }
