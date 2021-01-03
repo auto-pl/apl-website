dc8cd0acaf4963112ff75411d7ce194ba8da8807
SMC242 <smc242@users.noreply.github.com>
2021-01-03 18:02:52 +0000

    Fixed consistency in sizing styles

--- src/Components/containers/text_container.tsx
+++ src/Components/containers/text_container.tsx
@@ -30,7 +30,7 @@ type Height =
 /**
  * A percentage of the screen
  */
-type Width = 5 | 10 | 15 | 20 | 25 | 30 | 40 | 45 | 50 | 55 | 60;
+type Width = 10 | 20 | 30 | 40 | 50 | 60;
 
 /**
  * Measured in vh
