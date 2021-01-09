12c0fb9f3ff8b5b972c400941bac7f31b732fc97
SMC242 <smc242@users.noreply.github.com>
2021-01-09 11:10:57 +0000

    Worked out the problem
    The `tagName` argument wasn't required but was required to make the tooltip appear in the correct place >={
    
    Now there is a problem of the library adding styles to my buttons. I'm looking at how to counter this

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { Button, ToggleButton } from "../Components/inputs/buttons/ps2button";
+import Tooltip from "react-tooltip-lite";
+import {
+  PS2Button,
+  PS2ToggleButton,
+} from "../Components/inputs/buttons/ps2button";
 
 import { api } from "../Utils/api_interface";
 const conts = api.get_all_continents();
@@ -17,19 +21,41 @@ function App() {
         {/*
           <Map continents={conts} view_urls={["", "", "", ""]} />
         */}
-        <Button
+        <PS2Button
           text="test"
           on_click={() => console.log("Clicked")}
           tooltip_text="Hovered"
           value="yes"
         />
-        <Button />
-        <ToggleButton
+        <PS2Button />
+        <PS2ToggleButton
           text="toggle test"
           on_click={() => console.log("toggle clicked")}
           on_value="v"
           off_value="5"
         />
+        <Tooltip tagName="button" content={<div>Join MUMS</div>}>
+          <button>Join MUMS</button>
+        </Tooltip>
+        <Tooltip
+          content={
+            <div>
+              <h4 className="tip-heading">
+                An unordered list to demo some html content
+              </h4>
+              <ul className="tip-list">
+                <li>One</li>
+                <li>Two</li>
+                <li>Three</li>
+                <li>Four</li>
+                <li>Five</li>
+              </ul>
+            </div>
+          }
+          tagName="span"
+        >
+          Target content for big html tip
+        </Tooltip>
       </div>
     </div>
   );
