0a613dd2ab216e1a57365454d6d0bb999b8dccd3
SMC242 <smc242@users.noreply.github.com>
2021-01-16 15:30:13 +0000

    WIP // testing withbaseHoverMenu

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import {
   PS2Button,
   PS2ToggleButton,
 } from "../Components/inputs/buttons/ps2button";
+import { withBaseHoverMenu } from "../Components/HOCs/hover";
 import "../styles/global/ps2_styles/text.css";
 import "../styles/global/ps2_styles/background.css";
 import "../styles/global/ps2_styles/containers.css";
@@ -12,6 +13,11 @@ import "../styles/global/ps2_styles/positioning.css";
 
 import { api } from "../Utils/api_interface";
 const conts = api.get_all_continents();
+const base_state = conts[0].base_states[0];
+
+const HoverTest = withBaseHoverMenu(({}) => (
+  <TextContainer>Base hover HOC test</TextContainer>
+));
 
 function App() {
   // Set the title after the component is mounted
@@ -21,6 +27,7 @@ function App() {
 
   return (
     <div className="background wrapper">
+      <div id="portals"></div>
       {/* put your tests here */}
       <div className="left" id="testing-code-goes-here">
         <TextContainer
@@ -32,6 +39,7 @@ function App() {
           Cobaltium.
         </TextContainer>
         <PS2Button tooltip_text="join them">MUMS</PS2Button>
+        <HoverTest base_state={base_state} />
       </div>
     </div>
   );
