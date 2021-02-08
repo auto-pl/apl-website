6f03a838b8bdb5aa17e0f65cb0863992971b08ef
SMC242 <smc242@users.noreply.github.com>
2021-02-08 19:08:39 +0000

    Working on zoom

--- src/Components/wrappers/zoom.tsx
+++ src/Components/wrappers/zoom.tsx
@@ -1,12 +1,31 @@
-import React, { FC, ReactNode } from "react";
-import { ZoomHandler } from "../menus/zoom_controls";
+import React, { FC, ReactNode, ComponentType } from "react";
+import { ZoomControls } from "../menus/zoom_controls";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
-export interface ZoomProps {
+export interface WithZoomProps {
   /**
    * These will be wrapped in a div whose dimensions and offsets are changed.
    */
   children: ReactNode;
 }
 
-const zoom_in: ZoomHandler = (zoom_level) => {};
-//
+type TransformCallback = () => {};
+type TransformArgs = {
+  zoomIn: TransformCallback;
+  zoomOut: TransformCallback;
+  resetTransform: TransformCallback;
+};
+
+export function withZoom<TargetProps extends WithZoomProps>(
+  TargetComponent: ComponentType<TargetProps>
+): FC<TargetProps> {
+  const WrappedComponent = (props: TargetProps) => {
+    return (
+      <TransformWrapper>
+        {({ zoomIn, zoomOut, resetTransform }: TransformArgs) => {
+          <></>;
+        }}
+      </TransformWrapper>
+    );
+  };
+}
