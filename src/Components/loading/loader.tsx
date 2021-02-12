5f587e30aa683a9e116a93947223c3abac3292da
SMC242 <smc242@users.noreply.github.com>
2021-02-12 09:53:03 +0000

    Added className prop

--- src/Components/loading/loader.tsx
+++ src/Components/loading/loader.tsx
@@ -5,13 +5,19 @@ export interface PS2LoaderProps {
   loading: boolean;
   size?: number;
   style?: CSSProperties;
+  class_name?: string;
 }
 
 const COLOUR = "rgba(185, 190, 190, 0.9)";
 
-export const PS2Loader: FC<PS2LoaderProps> = ({ loading, size, style }) => {
+export const PS2Loader: FC<PS2LoaderProps> = ({
+  loading,
+  size,
+  style,
+  class_name,
+}) => {
   return (
-    <div style={style}>
+    <div className={class_name} style={style}>
       <PropagateLoader color={COLOUR} loading={loading} size={size} />
     </div>
   );
