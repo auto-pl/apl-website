c2ab87d83113bef2c34dcabdae9ee8f36849fe0b
SMC242 <smc242@users.noreply.github.com>
2021-02-17 07:37:58 +0000

    More storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,10 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-links",
+    "@storybook/addon-docs",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
-    "@storybook/addon-docs ",
   ],
   typescript: {
     check: false,
