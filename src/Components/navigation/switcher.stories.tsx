0aa10ce70f646854318e69aff4494618e520c7d5
SMC242 <smc242@users.noreply.github.com>
2021-02-17 09:53:35 +0000

    Switcher docs

--- src/Components/navigation/switcher.stories.tsx
+++ src/Components/navigation/switcher.stories.tsx
@@ -5,6 +5,22 @@ import { Switcher, SwitcherProps } from "./switcher";
 const meta: Meta = {
   title: "Switcher",
   component: Switcher,
+  argTypes: {
+    items: {
+      description: "The elements contained within the switcher",
+      table: {
+        type: {
+          summary: "{text: string, body: ReactNode}",
+          detail:
+            "text: the text on the item. This will be used as the switcher header when selected.\n\nbody: The element to display inside the switcher. ",
+        },
+      },
+      control: "object",
+    },
+    header_text: {
+      control: "text",
+    },
+  },
 };
 
 export default meta;
@@ -13,8 +29,8 @@ const Template: Story<SwitcherProps> = (args) => <Switcher {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   items: [
-    { name: "Item 1", body: <span>Item 1</span> },
-    { name: "Item 2", body: <span>Item 2</span> },
-    { name: "Item 3", body: <span>Item 3</span> },
+    { text: "Item 1", body: <span>Item 1</span> },
+    { text: "Item 2", body: <span>Item 2</span> },
+    { text: "Item 3", body: <span>Item 3</span> },
   ],
 };
