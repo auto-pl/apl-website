1bd56e8fb1795cc53b63c6a1ee680ef4de0814fc
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-02-28 23:38:40 +0000

    Made most button classes
    Just notched-corners to go

--- src/Components/inputs/buttons/ps2button.stories.tsx
+++ src/Components/inputs/buttons/ps2button.stories.tsx
@@ -1,6 +1,6 @@
 import React, { MouseEvent } from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
-import { PS2Button, PS2ButtonProps, NotchLocation } from "./ps2button";
+import { PS2Button, PS2ButtonProps, ButtonType } from "./ps2button";
 
 export default {
   title: "PS2Button",
@@ -25,7 +25,7 @@ const Template: Story<PS2ButtonProps> = ({ children, ...args }) => (
   <PS2Button {...args}>{children}</PS2Button>
 );
 
-export const Example = Template.bind({});
+export const CornerNotch = Template.bind({});
 const ayaya_add = (e: MouseEvent) => {
   const ele = document.querySelector("#ayaya-content");
   if (!ele) return;
@@ -33,11 +33,11 @@ const ayaya_add = (e: MouseEvent) => {
   console.log((e?.target as HTMLButtonElement).value);
 };
 
-Example.args = {
+CornerNotch.args = {
   children: <span id="ayaya-content"> </span>,
   tooltip_text:
     "This button will add an 'ayaya' to the child element each time it is clicked. Also logs the value of the button.",
-  notch_location: NotchLocation.TOP_LEFT,
+  notch_location: ButtonType.NOTCHED_CORNERS,
   onClick: ayaya_add,
   id: "button-hehe",
   value: "I am a teapot, short and stout",
@@ -49,7 +49,28 @@ Disabled.args = {
   children: "I am disabled",
   tooltip_text:
     "This button is disabled and cannot be clicked. Even its `onClick` will not fire",
-  notch_location: NotchLocation.TOP_LEFT,
+  notch_location: ButtonType.NOTCHED_CORNERS,
   onClick: () => console.log("I am not disabled :)"),
   disabled: true,
+};
+
+export const LeftNotch = Template.bind({});
+LeftNotch.args = {
+  children: "I have a notch on the left",
+  tooltip_text: "There is nothing special here. Just a different style.",
+  notch_location: ButtonType.NOTCHED_TOP_LEFT,
+};
+
+export const NoNotch = Template.bind({});
+NoNotch.args = {
+  children: "I have no notch",
+  tooltip_text: "There is nothing special here. Just a different style.",
+  notch_location: ButtonType.NO_NOTCH,
+};
+
+export const NoSides = Template.bind({});
+NoSides.args = {
+  children: "I have no notch and no sides",
+  tooltip_text: "There is nothing special here. Just a different style.",
+  notch_location: ButtonType.NO_SIDES,
 };
