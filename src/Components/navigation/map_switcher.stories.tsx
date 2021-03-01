0029bf57997a35947eda7dc52ed6abb26a3f8caf
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-03-01 22:26:06 +0000

    WIP // MapSwitcher story

---
+++ src/Components/navigation/map_switcher.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { MapSwitcher, MapSwitcherProps } from "./map_switcher";
+import { api } from "../../Utils/api_interface";
+
+const meta: Meta = {
+  title: "Switchers/MapSwitcher",
+  component: MapSwitcher,
+};
+
+export default meta;
+
+const Template: Story<MapSwitcherProps> = (args) => <MapSwitcher {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+  continents: api.get_all_continents(),
+};
