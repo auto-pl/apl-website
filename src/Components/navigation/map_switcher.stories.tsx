ba27340d7405c16e0ad461fa5adb90be37fc6959
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-03-10 15:55:33 +0000

    Moving towards a single page design

--- src/Components/navigation/map_switcher.stories.tsx
+++ src/Components/navigation/map_switcher.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 import { MapSwitcher, MapSwitcherProps } from "./map_switcher";
-import { to_continent_view } from "../../interfaces/continent";
 import { api } from "../../Utils/api_interface";
 
 const meta: Meta = {
@@ -14,10 +13,6 @@ export default meta;
 const Template: Story<MapSwitcherProps> = (args) => <MapSwitcher {...args} />;
 export const Default = Template.bind({});
 Default.args = {
-  continent_views: to_continent_view(api.get_all_continents(), [
-    "foo",
-    "bar",
-    "baz",
-    "boo",
-  ]),
+  continents: api.get_all_continents(),
+  set_new_continent: () => {},
 };
--- src/Components/navigation/map_switcher.tsx
+++ src/Components/navigation/map_switcher.tsx
@@ -1,9 +1,6 @@
-import React, { useState, FC, useCallback } from "react";
-import {
-  ContinentDetails,
-  ContinentViews,
-  faction,
-} from "../../interfaces/continent";
+import React, { FC } from "react";
+import { ContinentDetails, faction } from "../../interfaces/continent";
+import { PS2Button, ButtonType } from "../inputs/buttons/ps2button";
 import { get_active_continent } from "../../Utils/apitils";
 import { faction_logos } from "../../Utils/globals/faction_globals";
 import classNames from "classnames";
@@ -13,24 +10,22 @@ import "../../styles/components/map_switcher/map_switcher.css";
 import "../../styles/global/ps2_styles/positioning.css";
 import "../../styles/global/ps2_styles/text.css";
 
-export interface MapSwitcherProps {
-  continent_views: ContinentViews;
-}
+type ContinentChangeHandler = (cont: ContinentDetails) => void;
 
-type LockChecker = (continent: ContinentDetails) => boolean;
+export interface MapSwitcherProps {
+  continents: Array<ContinentDetails>;
+  /**
+   * This function should handle displaying the new continent
+   */
+  set_new_continent: ContinentChangeHandler;
+}
 
 interface ContinentItemProps {
   /**
    * All relevant information about the continent to link to
    */
-  continent_record: ContinentDetails;
-
-  /**
-   * The URL to the target continent's view
-   */
-  url: string;
-
-  check_locked: LockChecker;
+  continent: ContinentDetails;
+  set_new_continent: ContinentChangeHandler;
 }
 
 const continent_item_classes = (locked_by: faction) => {
@@ -46,55 +41,58 @@ interface FactionIMGProps {
 }
 
 const FactionIMG: FC<FactionIMGProps> = ({ locked_by }) => {
-  const logo_size = "32px";
   return locked_by ? (
     <img
       className="faction-logo"
       alt={`${locked_by} logo`}
       src={faction_logos.get(locked_by)}
-      height={logo_size}
-      width={logo_size}
     />
   ) : null;
 };
 
 const ContinentItem: FC<ContinentItemProps> = ({
-  continent_record,
-  url,
-  check_locked,
+  continent,
+  set_new_continent,
 }) => {
-  const { locked_by, name } = continent_record;
+  const { locked_by, name } = continent;
 
   return (
-    <div
+    <PS2Button
       className={get_class_names(
-        "continent-item",
+        "continent-item pair-text-image",
         continent_item_classes
       )(locked_by)}
+      onClick={set_new_continent}
+      button_type={ButtonType.NO_SIDES}
+      disabled={!!locked_by}
     >
-      <a className="font-primary pair-text-image" href={url}>
-        <FactionIMG locked_by={locked_by} />
-        {name}
-      </a>
-    </div>
+      <FactionIMG locked_by={locked_by} />
+      {name}
+    </PS2Button>
   );
 };
 
-export const MapSwitcher: FC<MapSwitcherProps> = ({ continent_views }) => {
+export const MapSwitcher: FC<MapSwitcherProps> = ({
+  continents,
+  set_new_continent,
+}) => {
   const active_cont = get_active_continent();
   const check_locked = (c: ContinentDetails): boolean => !!c.locked_by;
+  // [...continent] makes a copy to prevent mutating the prop
+  const sorted_continents = [...continents].sort((last, next) =>
+    next.name > last.name ? -1 : 1
+  );
 
-  const items = continent_views.map((cont, i) => ({
-    text: cont.details.name,
+  const items = sorted_continents.map((cont, i) => ({
+    text: cont.name,
     body: (
       <ContinentItem
-        continent_record={cont.details}
-        url={cont.view_url}
+        continent={cont}
         key={i}
-        check_locked={check_locked}
+        set_new_continent={set_new_continent}
       />
     ),
-    disabled: check_locked(cont.details),
+    disabled: check_locked(cont),
   }));
 
   return (
