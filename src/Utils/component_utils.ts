a49e2fdc8922ab45896e830999daa3c27cc6c994
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-03-17 11:17:49 +0000

    Fixed the recursion

--- src/Utils/component_utils.ts
+++ src/Utils/component_utils.ts
@@ -11,14 +11,19 @@ export const get_class_names = (
     [decide_variable_classes(...class_values), constant_classes].join(" ");
 };
 
-type AppendFunc = <T extends Array<any>, Z extends any>(
-  list: T,
-  to_append: Z
-) => [...T, Z];
-const append: AppendFunc = (list, to_append) => list.slice().concat(to_append);
+type AppendFunc = <T extends any>(
+  list: Array<T>,
+  to_append: Array<T>
+) => Array<T>;
+const append: AppendFunc = (list, to_append) => [...list, ...to_append];
 const range = (start: number, end: number): Array<number> => {
-  const _range = (current: number, end: number): number | Array<number> =>
-    current < end ? append([current], _range(current + 1, end) as number) : [];
-  return range(start, end) as Array<number>;
+  // I used a nested function to clean up the interface.
+  // Otherwise, the interface would be (number, number) => number | Array<number>
+  // which would suck to work with.
+  const _range = (current: number, upper: number): number | Array<number> =>
+    current < upper
+      ? append([current], _range(current + 1, upper) as Array<number>)
+      : [];
+  return _range(start, end) as Array<number>;
 };
 export const get_keys = (length: number): Array<number> => range(0, length);
