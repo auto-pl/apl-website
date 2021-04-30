c8e91353b01ea21ad54a5b84c211a3245c3d5b45
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-04-30 11:28:56 +0100

    Starting on API wrapper

---
+++ api_wrapper/Base/index.ts
@@ -0,0 +1,6 @@
+import { base_from_id } from "./base_from_id";
+
+const BaseAPI = {
+  base_from_id,
+};
+export default BaseAPI;
---
+++ api_wrapper/api_barrell.ts
@@ -0,0 +1,5 @@
+import BaseAPI from "./Base/index";
+// Every function should have its own file, which are exported in an object from `index.ts` in its folder
+
+const API = { BaseAPI };
+export default API;
