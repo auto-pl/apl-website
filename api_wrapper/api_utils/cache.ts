75fbc522dc74b60c1cf70da03474c94a2d6be237
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-05-05 11:27:50 +0100

    Cache wrapper

---
+++ api_wrapper/api_utils/cache.ts
@@ -0,0 +1,22 @@
+type KeyCompatible = string | number;
+/**
+ * Wrap the given function in a permanent cache.
+ * All results of the function will be cached until reload.
+ * @param func The function to wrap. The argument must be a string or number.
+ * @returns The wrapped function
+ */
+const with_cache = <KeyType extends KeyCompatible, ReturnType>(
+  func: (arg: KeyType) => ReturnType
+): ((arg: KeyType) => ReturnType) => {
+  const cache: Record<any, ReturnType> = {};
+  return (arg) => {
+    if (!cache[arg]) {
+      const result = func(arg);
+      cache[arg] = result;
+      return result;
+    }
+    return cache[arg];
+  };
+};
+
+export default with_cache;
