9c7e7235b7c33428be4c3ff0bcaf5e04c17a21c4
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-05-05 11:27:40 +0100

    Fixed typo

--- api_wrapper/api_barrell.ts
+++ api_wrapper/api_barrell.ts
@@ -1,5 +1,5 @@
 import BaseAPI from "./Base/index";
-// Every function should have its own file, which are exported in an object from `index.ts` in its folder
+// Every function should have its own file, which is exported in an object from `index.ts` in its folder
 
 const API = { BaseAPI };
 export default API;
