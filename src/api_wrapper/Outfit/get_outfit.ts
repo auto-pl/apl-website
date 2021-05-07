d19c2f9edbdb552171ce2c74ca1e73d927fb4dd7
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-05-07 10:51:38 +0100

    WIP // outfit getter

---
+++ src/api_wrapper/Outfit/get_outfit.ts
@@ -0,0 +1,6 @@
+import { OutfitInfo } from "../../interfaces/outfit";
+import with_cache from "../api_utils/with_cache";
+
+const _get_outfit = (id: bigint) => {};
+
+export default get_outfit;
---
+++ src/api_wrapper/Outfit/index.ts
@@ -0,0 +1,5 @@
+import { get_outfit } from "./get_outfit";
+
+const OutfitAPI = { get_outfit };
+
+export default OutfitAPI;
