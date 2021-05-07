361bdb7c066c4e3cc9886e6ed4facc6c28a521a6
Leonhard S <leonhard-sei@outlook.com>
2021-05-07 18:59:04 +0200

    Add comment
    This was not picked for elegance but because VS Code understands it.

--- src/interfaces/api_interfaces/continent.ts
+++ src/interfaces/api_interfaces/continent.ts
@@ -33,6 +33,7 @@ export type APIBaseStatus = Readonly<_BaseStatus>;
 interface _ContinentInfo {
   id: number;
   name: string;
+  /** Internal identifier used as part of API file names. */
   code: string;
   description: string;
   lattice_links: Array<BaseCoords>; // Numbers are base IDs
