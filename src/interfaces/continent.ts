62cb8eb4f8a9ac2a256148f54aa950e25fb0fcd7
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-05-12 12:29:31 +0100

    Outfit ownership is optional

--- src/interfaces/continent.ts
+++ src/interfaces/continent.ts
@@ -97,6 +97,6 @@ interface _BaseStatus extends Omit<APIBaseStatus, "owning_outfit"> {
   server_id: Servers;
   owning_faction?: Factions;
   owning_outfit: () => Promise<OutfitInfo>;
-  owning_outfit_id: bigint;
+  owning_outfit_id?: bigint;
 }
 export type BaseStatus = Readonly<_BaseStatus>;
