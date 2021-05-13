08c8412f82410daea9a0bc0118f66b142f480cb4
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-05-13 10:36:19 +0100

    WIP // outfit query

--- src/api_wrapper/Outfit/get_outfit.ts
+++ src/api_wrapper/Outfit/get_outfit.ts
@@ -1,6 +1,11 @@
 import { OutfitInfo } from "../../interfaces/outfit";
-import with_cache from "../api_utils/with_cache";
+import { with_cache } from "../api_utils/cache";
 
-const _get_outfit = (id: bigint) => {};
+const _get_outfit = async (id: string): Promise<OutfitInfo> => {
+  // TODO: query outfit endpoint
+  // TODO: call converter
+  return {} as OutfitInfo;
+};
 
+const get_outfit = with_cache()(_get_outfit);
 export default get_outfit;
