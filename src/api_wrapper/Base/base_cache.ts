9d37361dcdfc912531d106e67ee24865507b2be3
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-05-19 12:40:48 +0100

    BaseCache holds promises

--- src/api_wrapper/Base/base_cache.ts
+++ src/api_wrapper/Base/base_cache.ts
@@ -1,4 +1,4 @@
 import { BaseInfo } from "../../interfaces/continent";
 import { make_cache, AbstractCache } from "../api_utils/cache";
 
-export const BaseCache = make_cache() as AbstractCache<BaseInfo>;
+export const BaseCache = make_cache() as AbstractCache<Promise<BaseInfo>>;
