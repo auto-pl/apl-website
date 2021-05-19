21be9b4859be8058c35cb19edf2254df9821bd7d
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-05-19 12:41:18 +0100

    Barrelled the base getters

--- src/api_wrapper/Base/index.ts
+++ src/api_wrapper/Base/index.ts
@@ -1,10 +1,14 @@
 import { BaseCache } from "./base_cache";
 import { convert_base_info } from "./base_info_converters";
 import { convert_base_status } from "./base_status_converters";
+import { get_base_status } from "./get_base_status";
+import { get_base_info } from "./get_base_info";
 
 const BaseAPI = {
   BaseCache,
   convert_base_info,
   convert_base_status,
+  get_base_status,
+  get_base_info,
 };
 export default BaseAPI;
