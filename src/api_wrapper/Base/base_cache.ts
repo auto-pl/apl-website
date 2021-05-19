3bed6dcf01ad3993ce95abd7fcb5265f317a50f8
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-05-19 12:22:11 +0100

    Named export for cache

--- src/api_wrapper/Base/base_cache.ts
+++ src/api_wrapper/Base/base_cache.ts
@@ -1,5 +1,4 @@
 import { BaseInfo } from "../../interfaces/continent";
 import { make_cache, AbstractCache } from "../api_utils/cache";
 
-const BaseCache = make_cache() as AbstractCache<BaseInfo>;
-export default BaseCache;
+export const BaseCache = make_cache() as AbstractCache<BaseInfo>;
