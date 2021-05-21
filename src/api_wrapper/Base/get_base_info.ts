8276cd9a4e6b43bde636e21a54c6711941e341fe
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-05-21 09:46:04 +0100

    BaseInfo query

--- src/api_wrapper/Base/get_base_info.ts
+++ src/api_wrapper/Base/get_base_info.ts
@@ -1,11 +1,33 @@
 import { BaseInfo } from "../../interfaces/continent";
+import { APIBaseInfo } from "../../interfaces/api_interfaces/continent";
 import { with_cache } from "../api_utils/cache";
 import { BaseCache } from "./base_cache";
+import { convert_base_info } from "./base_info_converters";
+import { QueryAPI } from "../Query";
+const { query } = QueryAPI;
+
+const is_base_info = (result: unknown): result is APIBaseInfo => {
+  if (!(result instanceof Object)) return false;
+  const required_keys = [
+    "name",
+    "continent_id",
+    "id",
+    "map_pos",
+    "type_id",
+    "type_name",
+    "resource_amount",
+  ];
+  return required_keys.map((k) => k in result).every((e) => e);
+};
 
 const _get_base_info = async (id: string): Promise<BaseInfo> => {
-  // TODO: query API for BaseInfo
-  // TODO: convert repsonse
-  return {} as BaseInfo;
+  const params: Array<Record<string, string>> = [{ id }];
+  const result = await query(["base", "info"])(params);
+  if (!is_base_info(result))
+    throw new Error(
+      `"Got invalid \`APIBaseInfo\` response from APL API. Notify the API team. Result: ${result}`
+    );
+  return convert_base_info(result);
 };
 
 export const get_base_info = with_cache(_get_base_info, BaseCache);
